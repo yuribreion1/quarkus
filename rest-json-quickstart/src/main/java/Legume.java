71f76637ff2f2a93992ea5832dbd0919777af61e
Yuri Breion <yuribreion@gmail.com>
2022-09-10 17:24:25 -0300

    JSON and REST training

---
+++ rest-json-quickstart/src/main/java/Legume.java
@@ -0,0 +1,13 @@
+public class Legume {
+
+    public String name;
+    public String description;
+
+    public Legume() {
+    }
+
+    public Legume(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
